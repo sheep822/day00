3883650d959d2eea8fb0bec81dc27d7ca8a1cddf
xiaoxin-aosi <3042874090@qq.com>
2020-12-15 20:30:52 +0800

    vf

--- app/src/main/java/com/garden/day00/MainActivity.java
+++ app/src/main/java/com/garden/day00/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		//爱是非法诶哦
+		//摄像头一个就
+		
     }
 }
