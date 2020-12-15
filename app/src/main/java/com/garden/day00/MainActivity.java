db75ccdb39bace9ee1c89d03f7850d1da024654a
SunDreamFly <1819163694@qq.com>
2020-12-15 20:08:44 +0800

    这是我新上传的

--- app/src/main/java/com/garden/day00/MainActivity.java
+++ app/src/main/java/com/garden/day00/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		//别看了 是我啊
     }
 }
